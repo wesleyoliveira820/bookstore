a41a4298ed6962d1d44fa83dc2f5a9ec58aefd99
wesleyoliveira820 <wesleyoliveiradeveloper@gmail.com>
2021-03-11 01:36:15 -0300

    feat: [client] added loader and option disabled on button component

--- packages/client/src/components/Button/index.tsx
+++ packages/client/src/components/Button/index.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react";
-import { Container } from "./styles";
+import { Container, Loader } from "./styles";
 
 interface IButtonProps {
   onClick?: () => void;
+  isLoading?: boolean;
+  disabled?: boolean;
 }
 
-const Button: FC<IButtonProps> = ({ children, onClick }) => (
-  <Container onClick={onClick}>{children}</Container>
+const Button: FC<IButtonProps> = ({
+  children,
+  onClick,
+  isLoading = false,
+  disabled = false,
+}) => (
+  <Container onClick={onClick} disabled={disabled}>
+    {isLoading ? <Loader /> : children}
+  </Container>
 );
 
 export default Button;
--- packages/client/src/components/Button/styles.ts
+++ packages/client/src/components/Button/styles.ts
@@ -1,5 +1,10 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { lighten } from "polished";
+
+const rotationLoader = keyframes`
+  0% { transform: rotate(0deg);}
+  100% {transform: rotate(360deg);}
+`;
 
 export const Container = styled.button`
   width: 100%;
@@ -17,4 +22,14 @@ export const Container = styled.button`
     background-color: ${(props) =>
       lighten(0.05, props.theme.colors.brand.primary)};
   }
+`;
+
+export const Loader = styled.div`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 4px solid ${(props) => lighten(0.2, props.theme.colors.brand.primary)};
+  border-top: 4px solid ${(props) => props.theme.colors.bg.primary};
+  margin: auto;
+  animation: ${rotationLoader} 1s infinite linear;
 `;
